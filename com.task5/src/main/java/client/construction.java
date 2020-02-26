35b2347338aa64d485d2bc04c9ac5747f57e6fd0
Thanushareddy <48981064+Thanushareddy@users.noreply.github.com>
2020-02-26 22:15:16 +0530

    comitted successfully

---
+++ com.task5/src/main/java/client/construction.java
@@ -0,0 +1,6 @@
+package client;
+
+public interface construction {
+	String Constructions_Materials(String Material_Standard,int Area );
+
+}
---
+++ com.task5/src/main/java/player/presented.java
@@ -0,0 +1,8 @@
+package player;
+
+public interface presented {
+	int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+
+}
